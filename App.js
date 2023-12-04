dd577ccae3f969c2dd8845afe26a9d93de12c407
Woolleyjoseph <100800325+Woolleyjoseph@users.noreply.github.com>
2023-12-04 14:06:29 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/app/screens/Main';
+
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
+
